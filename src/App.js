0a39eae44429c26c02ffc551993bb214ec8443a8
Ahmad <khan@mail.ru>
2023-08-13 23:15:03 +0300

    modified with redux-toolkit

--- src/App.js
+++ src/App.js
@@ -7,72 +7,16 @@ import { Todos } from "./components/Todos"
 
 
 export const App = () => {
-const [todos, setTodos] = useState( [
-    {favorite: false, text: "Купить продукты"},
-    {favorite: false, text: "Купить бананы"},
-    {favorite: false, text: "Купить машину"},
-    {favorite: false, text: "Купить дом"},
-    {favorite: false, text: "Купить участок"}
-  ] )
-
 const [text, setText] = useState("")
-
-const deleteTodo = (indexOfDeletedItem) => {
-  const filterTodos = todos.filter((todo, index) => {
-    if (index === indexOfDeletedItem) {
-      return false
-    }
-    return true
-  })
-
-  setTodos(filterTodos)
-}
-
-
-const makeFavorite = (indexOfMakeFavorite) => {
-  const newTodos = todos.map((item, index) => {
-    if (index === indexOfMakeFavorite) {
-      return {
-        ...item,
-        favorite: !item.favorite
-      }
-    }
-
-    return item
-  })
-  setTodos(newTodos)
-}
-
-const addTodo = () => {
-
-  const newTodo = todos.find((todo) => todo.text)
-  if (newTodo.text === text || text === "") {
-    console.log("Это дело уже заявлено или оно пустое")
-  } else {
-    setTodos([ {
-      text: text,
-      favorite: false
-    },
-    ...todos
-  ])
-  setText("")
-  }
-}
-
 
   return (
     <div className="App">
       <Header />
       <Forms 
         text={text} 
-        addTodo={addTodo} 
         setText={setText}
       />
-      <Todos 
-        todos={todos}
-        makeFavorite={makeFavorite}
-        deleteTodo={deleteTodo}
-      />
+      <Todos />
     </div>
   );
 }
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,15 +1,18 @@
+import { useDispatch, useSelector } from "react-redux"
+import { makeFavorite, deleteTodo } from "../redux/TodoSlice";
 
 
 
-
-export const Todos = ({ todos, makeFavorite, deleteTodo }) => {
+export const Todos = () => {
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todo.todos)
     return (
         todos.map((todo, index) => {
             return(
               <div className="todos">
                  <div className={`todo ${todo.favorite ? "selected" : ""}`}>
                     <div className="favorite">
-                      <button onClick={() => makeFavorite(index)}>
+                      <button onClick={() => dispatch(makeFavorite({ index: index }))}>
                          ★
                       </button>
                     </div>
@@ -17,7 +20,7 @@ export const Todos = ({ todos, makeFavorite, deleteTodo }) => {
                      {todo.text}
                     </div>
                     <div className="actions">
-                      <button onClick={() => deleteTodo(index)}>&#10005;</button>
+                      <button onClick={() => dispatch(deleteTodo({ index: index }))}>&#10005;</button>
                     </div>
                  </div>
                 </div>
